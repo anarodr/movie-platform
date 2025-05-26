d29054223e9a6abdc90808068ce784678a6df7bf
Ana Rodriguez <anarodriguezvallejos@gmail.com>
2025-05-26 18:50:35 +0200

    build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
-    base: 'https://github.com/anarodr/movie-platform/frontend/',
+    base: 'https://github.com/anarodr/movie-platform/',
     plugins: [react()],
     test: {
         include: ['src/**/*.{test,spec}.{ts,tsx,js,jsx}'],
